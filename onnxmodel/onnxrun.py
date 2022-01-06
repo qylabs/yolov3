c77022cd28b3c1975bd515566d395b9fa0711baf
shihuiyu <56906943+shihuiyu@users.noreply.github.com>
2022-01-06 11:31:06 +0800

    Add files via upload
    修改了多分类画框的问题

--- onnxmodel/onnxrun.py
+++ onnxmodel/onnxrun.py
@@ -1,7 +1,10 @@
+import os
 import numpy as np
 import onnxruntime as ort
 import cv2
 import copy
+import torch
+import torchvision
 
 strides=[16,32] #
 
@@ -37,7 +40,7 @@ def NMS(dets,threshold):
     # print('area ',area)
     #sort the idx rather val
     order=scores.argsort()[::-1]#::-1 逆序
-    # print('order ',order)
+    print('order ',order)
 
     tmp=[]
     while order.size>0:
@@ -57,7 +60,7 @@ def NMS(dets,threshold):
         # print('inter ',inter,inter.shape)
         #iou
         iou=inter/(area[i]+area[order[1:]]-inter)
-        # print('iou ',iou)
+        print('iou ',iou)
         
         #找到重叠度不高于阈值的矩形框索引
         idx=np.where(iou<=threshold)[0] #this idx is iou idx, 1 smaller than org dets
@@ -99,7 +102,7 @@ def plot_one_box(x, im, img_save_path,color=(128, 128, 128), label=None, line_th
     cv2.imwrite(img_save_path,im)
 
 def non_max_suppression(prediction, conf_thres=0.1, iou_thres=0.45, classes=None, agnostic=False, multi_label=False,
-                        labels=(), max_det=300,iter=False):
+                        labels=(), max_det=300,iter=True):
     """Runs Non-Maximum Suppression (NMS) on inference results
 
     Returns:
@@ -109,8 +112,10 @@ def non_max_suppression(prediction, conf_thres=0.1, iou_thres=0.45, classes=None
     assert 0 <= conf_thres <= 1, f'Invalid Confidence threshold {conf_thres}, valid values are between 0.0 and 1.0'
     assert 0 <= iou_thres <= 1, f'Invalid IoU {iou_thres}, valid values are between 0.0 and 1.0'
 
-    # nc = prediction.shape[2] - 5  # number of classes
-    nc=5
+    nc = prediction.shape[2] - 5  # number of classes
+    # print('prediction.shape:',prediction.shape)
+    # print('nc=',nc)
+    # nc=7
     xc = prediction[..., 4] > conf_thres  # candidates
     # prediction=prediction[xc] #filter out low confidence
     # print('xc=',xc)
@@ -120,8 +125,9 @@ def non_max_suppression(prediction, conf_thres=0.1, iou_thres=0.45, classes=None
     max_nms = 3000  # maximum number of boxes
     # time_limit = 10.0  # seconds to quit after
     # redundant = True  # require redundant detections
-    # multi_label &= nc > 1  # multiple labels per box (adds 0.5ms/img)
-    # merge = False  # use merge-NMS
+    multi_label &= nc > 1  # multiple labels per box (adds 0.5ms/img)
+    # print('multi_label is:', multi_label)
+    merge = False  # use merge-NMS
 
     ##support batch-images
     output=[[]]*prediction.shape[0]
@@ -150,19 +156,32 @@ def non_max_suppression(prediction, conf_thres=0.1, iou_thres=0.45, classes=None
         else:
             # Compute conf
             x[:, 5:] *= x[:, 4:5]  # conf = obj_conf * cls_conf
+            # print('1-2 x=',x)
             # Box (center x, center y, width, height) to (x1, y1, x2, y2)
             box = xywh2xyxy(x[:, :4])
+            # print('1 box=', box)
 
             # Detections matrix nx6 (xyxy, conf, cls)
             if multi_label:
-                i, j = (x[:, 5:] > conf_thres).nonzero(as_tuple=False).T
+                """ nnn = (x[:, 5:] > conf_thres)
+                print('nnn is: ', nnn)
+                nnnnn = nnn.nonzero()
+                print('nnnnn is:', nnnnn)
+                nnnnn2 = nnn.nonzero(as_tuple=False)
+                print('nnnnn2 is:', nnnnn2)
+                j, i = nnnnn
+                print('i = ', i, 'j = ', j) """
+                j, i = (x[:, 5:] > conf_thres).nonzero() #.T  #as_tuple=False
                 x = torch.cat((box[i], x[i, j + 5, None], j[:, None].float()), 1)
             else:  # best class only
                 conf = x[:, 5:].max(1, keepdims=True)
+                # print('conf =', conf)
                 j=x[:,5:].argmax(1).reshape(conf.shape) #cls num
                 x = np.concatenate((box, conf, j.astype(conf.dtype)), 1)
+                # print('1-3 x=',x)
                 conf_mask=x[:,4]>conf_thres
                 x=x[conf_mask]
+                # print('1-4 x=',x)
         # print('2x',x)
         # Filter by class
         if classes is not None:
@@ -179,28 +198,95 @@ def non_max_suppression(prediction, conf_thres=0.1, iou_thres=0.45, classes=None
         elif n > max_nms:  # excess boxes
             x = x[x[:, 4].argsort(descending=True)[:max_nms]]  # sort by confidence
         
-        # Batched NMS
-        # c = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
-        # boxes, scores = x[:, :4] + c, x[:, 4]  # boxes (offset by class), scores
-        # i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
-        # if i.shape[0] > max_det:  # limit detections
-        #     i = i[:max_det]
-        # if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
-        #     # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
-        #     iou = box_iou(boxes[i], boxes) > iou_thres  # iou matrix
-        #     weights = iou * scores[None]  # box weights
-        #     x[i, :4] = torch.mm(weights, x[:, :4]).float() / weights.sum(1, keepdim=True)  # merged boxes
-        #     if redundant:
-        #         i = i[iou.sum(1) > 1]  # require redundancy
+        """ # Batched NMS
+        c = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
+        boxes, scores = x[:, :4] + c, x[:, 4]  # boxes (offset by class), scores
+        i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
+        if i.shape[0] > max_det:  # limit detections
+            i = i[:max_det]
+        if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
+            # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
+            iou = box_iou(boxes[i], boxes) > iou_thres  # iou matrix
+            weights = iou * scores[None]  # box weights
+            x[i, :4] = torch.mm(weights, x[:, :4]).float() / weights.sum(1, keepdim=True)  # merged boxes
+            if redundant:
+                i = i[iou.sum(1) > 1]  # require redundancy
 
-        # output[xi] = x[i]
-        # if (time.time() - t) > time_limit:
-        #     print(f'WARNING: NMS time limit {time_limit}s exceeded')
-        #     break  # time limit exceeded
-        keep_dets=NMS(x,iou_thres)
-        # print('keep_dets',keep_dets)
+        output[xi] = x[i]
+        if (time.time() - t) > time_limit:
+            print(f'WARNING: NMS time limit {time_limit}s exceeded')
+            break  # time limit exceeded """
+        class_n = set(x[:,5]) 
+        print('class_n is:', class_n)
+        len_c = len(class_n)
 
-        output[xi]=x[keep_dets]
+        if len_c == 1:
+
+            keep_dets=NMS(x,iou_thres)
+            # print('keep_dets',keep_dets)
+            output[xi]=x[keep_dets]
+            # print('3 output is:', output)
+
+        else:
+            x_c1 = np.zeros((int(x.shape[0]), int(x.shape[1])))
+            # x_c1 = [[]]
+            x_c2 = np.zeros((int(x.shape[0]), int(x.shape[1])))
+            # print(' before x_c1 = ', x_c1)
+            # print(' before x_c2 = ', x_c2)
+            class_1 = x[0,5]
+            # print('class_l = ',class_1)
+            index = -1
+            for j in x[:,5]:
+                
+                index += 1
+                # print('index = ', index)
+                if j == class_1:
+                    """ print('j = ', j)
+                    print('x_c1 = ', x_c1)
+                    print('x[index,:] = ', x[index,:])
+                    print('x_c1[index,:] = ', x_c1[index,:]) """
+                    aaaa = x[index,:]
+                    # print('aaaa = ', aaaa)
+
+                    x_c1[index,:] += aaaa
+                    # print('x_c1[index,:] = ', x_c1[index,:])
+                    # x_c1 =[x_c1, x[index,:]]
+                    # x_c1.append(np.mat(x[index,:]))
+                    # print('x_c1 = ', x_c1)
+                else:
+                    """ print("!=")
+                    print('x_c2 = ', x_c2)
+                    print('x[index,:] = ', x[index,:])
+                    print('x_c2[index,:] = ', x_c2[index,:]) """
+                    aaaab = x[index,:]
+                    x_c2[index,:] = aaaab
+                    # x_c2 = [x_c2, x[index,:]]
+                    # print('x_c2 = ', x_c2)
+            # print('x_c1 = ', x_c1)
+            # print('x_c2 = ', x_c2)
+            xc1 = x_c1[..., 4] > conf_thres
+            x_c1 = x_c1[xc1]
+            xc2 = x_c2[..., 4] > conf_thres
+            x_c2 = x_c2[xc2]
+            # print('x_c1 = ', x_c1)
+            # print('x_c2 = ', x_c2)
+
+            keep_dets1=NMS(x_c1,iou_thres)
+            # print('keep_dets1',keep_dets1)
+            keep_dets2=NMS(x_c2,iou_thres)
+            # print('keep_dets2',keep_dets2)
+            output1 = x_c1[keep_dets1]
+            output2 = x_c2[keep_dets2]
+            # print('output1 = ', output1)
+            # print('output2 = ', output2)
+             
+            output_ = np.vstack([output1, output2])
+            # print('output_ is:', output_)
+            output[xi] = output_
+            # print('3 output is:', output)
+
+
+        
         # print('4 output.shape',output.shape)
 
     return output
@@ -443,6 +529,7 @@ def center2grid2(output,feat_nx,feat_ny,nc,anchors,strides):
         # print("after y[..., 2:4]=",y[..., 2:4])
         
         z.append(y.reshape(bs, na*feat_ny[i]*feat_nx[i], no))
+        # print('z is:', z)
         
     return np.concatenate(z,axis=1)
 
@@ -458,14 +545,16 @@ def center2grid2_iter(output,feat_nx,feat_ny,nc,anchors,strides,conf_thred=0.1):
     len(output)==nl
     '''
     bs=1 #batch_size
-    nl=len(anchors) #num head
-    na=len(anchors[0])//2 #num anchor
+    nl=len(anchors) #num head   2
+    # print('nl is:', nl) 
+    na=len(anchors[0])//2 #num anchor   3
+    # print('na is:', na)
     no=nc+5 #num output
     anchor_grid=make_anchor_grid(anchors)
     # print('anchor_gird.shape ',anchor_grid.shape)
     # print('anchor_gird\n ',anchor_grid)
-    feat_nx0=copy.deepcopy(feat_nx)
-    feat_ny0=copy.deepcopy(feat_ny)
+    feat_nx0=copy.deepcopy(feat_nx)     #[8,4]
+    feat_ny0=copy.deepcopy(feat_ny)     #[10,5]
     
     z=[]
     
@@ -473,8 +562,8 @@ def center2grid2_iter(output,feat_nx,feat_ny,nc,anchors,strides,conf_thred=0.1):
     for i in range(nl):
         print('================idx=',i)
         output_flat=output[i].flatten() #align with c code
-        feat_nx=int(feat_nx0[i])
-        feat_ny=int(feat_ny0[i])
+        feat_nx=int(feat_nx0[i])      #8
+        feat_ny=int(feat_ny0[i])      #10
         feat_map=feat_nx*feat_ny
         print('feat_map=',feat_map)
         # print('after sigmoid out_sigmoid=',sigmoid(output_flat))
@@ -517,7 +606,8 @@ def center2grid2_iter(output,feat_nx,feat_ny,nc,anchors,strides,conf_thred=0.1):
 
                     score*=sigmoid(output_flat[a*no*feat_map+(cls_out+5)*feat_map+feat])
                     z.append([x,y,w,h,score,cls_out])
-        # print('len(z)=',len(z))
+                    # print('z is:', z)
+        print('len(z)=',len(z))
         
     return np.concatenate(z)
 
@@ -532,9 +622,10 @@ def yolo_proc_simp_2H_noreshape_nosigmoid(img_path,onnx_path,strides,anchors,nc,
     input_shape=img0.shape
     # print('input_shape',input_shape)
     #preproc img
-    input_array=transfrom_img(img_path,gray_input=True)
+    input_array=transfrom_img(img_path,gray_input=True)  #(128,160)
     print(input_array.shape)
     out=onnxrun(onnx_path,input_array) #onnx-graph has only 1 output
+    # print('out is: ', out)
     print('==>> 2H output onnx out.shape ',len(out))
     print("out[0].shape, out[1].shape ",out[0].shape, out[1].shape)
     # print("out[0]==\n",out[0])
@@ -544,23 +635,23 @@ def yolo_proc_simp_2H_noreshape_nosigmoid(img_path,onnx_path,strides,anchors,nc,
     #postproc 
     #0. reshape+sigmoid
     #1. center2grid of onnx_sim_2H model
-    feat_nx=[int(input_shape[1]/s) for s in strides]
-    feat_ny=[int(input_shape[0]/s) for s in strides]
+    feat_nx=[int(input_shape[1]/s) for s in strides]  #strides[16,32] [8, 4]
+    feat_ny=[int(input_shape[0]/s) for s in strides]  #               [10,5]
     # print('feat_nx,feat_ny ',feat_nx,feat_ny)
     if iter is False:
         out1=center2grid2(out,feat_nx,feat_ny,nc,anchors,strides) #obtain the pred
-        # print('==>>final out1.shape',out1.shape)
+        print('==>>final out1.shape',out1.shape)
         # print("out1 ",out1)
         out=out1
     else:
         print("===================Second method====================")
         out2=center2grid2_iter(out,feat_nx,feat_ny,nc,anchors,strides).reshape(1,-1,6) #obtain the pred
-        # print('==>>final out2.shape',out2.shape)
+        print('==>>final out2.shape',out2.shape)
         # print("out2 ",out2)
         out=out2
 
     #2. nms out
-    pred = non_max_suppression(out, conf_thres,iou_thres,iter=iter)
+    pred = non_max_suppression(out, conf_thres,iou_thres,iter=False, multi_label=False)
     # print('pred_nms ',pred)
 
     # #plot
@@ -569,10 +660,11 @@ def yolo_proc_simp_2H_noreshape_nosigmoid(img_path,onnx_path,strides,anchors,nc,
     for i, det in enumerate(pred):  # detections per image
         print(i,', det',det)
         for *xyxy, conf, cls in det:
-            # print('xyxy ',xyxy)
+            # print('index is:', index)
+            print('xyxy ',xyxy)
             c = int(cls)  # integer class
             label_mark=f'{c}-{conf:.2f}'
-            # print('label_mark ',label_mark)
+            print('label_mark ',label_mark)
             plot_one_box(xyxy, img0, img_save_path,color=(2, 8, 255),label=label_mark)
     print('end')
 
@@ -586,7 +678,9 @@ if __name__=="__main__":
     # onnx_path='./runs/train/exp_yolov3_tiny3_gray_WP/weights/best_org.onnx'
     
 
-    img_path='./img_OUT_0_resize.ppm'
+    # img_path='/home/shihuiyu/dataset/origin_figures/img_OUT_100_6.ppm'
+    img_file='/home/shihuiyu/dataset/qiyuan_gesture_cleaned_resized/test/person15-l/'
+    img_path_list = os.listdir(img_file)
 
     # compute ONNX Runtime output prediction
     batch_size=1
@@ -613,6 +707,11 @@ if __name__=="__main__":
     # yolo_proc_simp_1H(img_path,onnx_path,strides,anchors,nc=5,conf_thres=0.2,iou_thres=0.45)
 
     print("==============================")
-    onnx_path='./runs/train/exp2/weights/qat_no_quant.onnx'
-    yolo_proc_simp_2H_noreshape_nosigmoid(img_path,onnx_path,strides,anchors,nc=1,conf_thres=0.2,iou_thres=0.45,iter=True)
+    # onnx_path='./runs/train/exp24/weights/best.onnx'
+    onnx_path='/home/shihuiyu/yolov3-master/runs/train/exp28/weights/best.onnx'
+
+    for imag_path in img_path_list:
+        img_path = img_file + imag_path
+        print('img_path is:', img_path)
+        yolo_proc_simp_2H_noreshape_nosigmoid(img_path,onnx_path,strides,anchors,nc=7,conf_thres=0.2,iou_thres=0.2,iter=False)  #iou 0.45
 
